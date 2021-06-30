a32374e9f1f418db45856fe65a4e2714c59f2b33
alecksalecks <alexburton112@gmail.com>
2021-06-30 21:12:51 +0100

    completed

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import json
 import pymongo
 import requests
 import time
-import os
+import concurrent.futures
+import random
 
 #API_KEY
 CONNECTION_STR = input("Enter your database connection string:") #e.g. mongodb://localhost:27017/
@@ -20,24 +21,30 @@ def get_all_sports():
     })
     return json.loads(sports_response.text)
 
-def get_all_fixtures():
-    all_fixtures_list = list()
-    for key in DATABASE["sports"].distinct('key'):
-        for region in REGIONS:
-            fixtures_response_loaded = json.loads(
+def get_all_fixtures_thread_function(key, region):
+    time.sleep(random.randint(0,2)) #to avoid "Requests are too frequent" error
+    return  json.loads(
                 requests.get('https://api.the-odds-api.com/v3/odds', params={
                     'api_key': API_KEY,
                     'sport' : key,
                     'region': region,
                     'mkt': 'h2h'
             }).text)
-            if fixtures_response_loaded['success']:
-                all_fixtures_list.append(fixtures_response_loaded['data'])
-            else:
-                print("FAILED",
-                    "Failed request to get fixtures: ",
-                    fixtures_response_loaded['msg'])
-                exit
+
+def get_all_fixtures():
+    all_fixtures_list = list()
+    for region in REGIONS: #could multithread this too
+        with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+            futures = [executor.submit(get_all_fixtures_thread_function, key, region) for key in DATABASE["sports"].distinct('key')]
+            return_value = [f.result() for f in futures]
+            for fixtures_response_loaded in return_value:
+                if fixtures_response_loaded['success']:
+                        all_fixtures_list.extend(fixtures_response_loaded['data'])
+                else:
+                    print("FAILED",
+                        "Failed request to get fixtures: ",
+                        fixtures_response_loaded['msg'])
+                    exit           
     return all_fixtures_list
 
 def get_all_live_fixtures(): #performs 30 queries at the moment but the API trial limit is 500 queries per month so be careful testing
@@ -70,22 +77,19 @@ def store_all_sports(sports_data):
     return True
 
 def store_all_fixtures(fixtures_all_json, fixtures_live_json): #only stores all not in play matches
-    upcoming_fixtures = list(
-        set(fixtures_all_json) 
-        - 
-        set(fixtures_live_json)
-        )
+    upcoming_fixtures = [x for x in fixtures_all_json if x not in fixtures_live_json]
+    upcoming_fixtures = filter(lambda x: len(x['sites']) > 0, upcoming_fixtures) #accounts for when no betting sites exist for the item
     for item in upcoming_fixtures:
         DATABASE["upcoming_fixtures"].insert_one({
-            "_id" : item['id'],
+            #"_id" : item['id'], #causes duplicate key errors
             "sport_key" : item['sport_key'],
             "sport_nice" : item['sport_nice'],
-            "team_0" : item['teams']['0'],
-            "team_1" : item['teams']['1'],
+            "team_0" : item['teams'][0],
+            "team_1" : item['teams'][1],
             "commence_time" : item['commence_time'],
             "home_team" : item['home_team'],
-            "fst_h2h_0" : item['sites']['0']['odds']['h2h']['0'], #get first h2h odds for this exercise, could get an average or store all of the h2h odds instead
-            "fst_h2h_1" : item['sites']['0']['odds']['h2h']['1']
+            "fst_h2h_0" : item['sites'][0]['odds']['h2h'][0], #get first h2h odds for this exercise, could get an average or store all of the h2h odds instead
+            "fst_h2h_1" : item['sites'][0]['odds']['h2h'][1]
         })
     return True
 
